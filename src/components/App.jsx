bd00139d661c8331a86b07d66549339b7f35767b
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2023-02-24 10:35:38 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
-export const App = () => {
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import Message from './Message/Message';
+import css from './App.module.css';
+
+const initialContacts = JSON.parse(localStorage.getItem('contacts')); 
+
+function App() {
+  const [contacts, setContacts] = useState(initialContacts || []);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    parsedContacts ? setContacts(parsedContacts) : setContacts([]);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const newContact = { id: nanoid(), name, number };
+
+    contacts.some(contact => contact.name === name)
+      ? Report.warning(
+          `${name}`,
+          'This user is already in the contact list.',
+          'OK',
+        )
+      : setContacts(prevContacts => [newContact, ...prevContacts]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId),
+    );
+  };
+
+  const changeFilter = e => setFilter(e.currentTarget.value);
+
+  const filtredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  const filterContacts = filtredContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+      <div className={css.container}>
+        <h1 className={css.title}>
+          Phone<span className={css.title__color}>book</span>
+        </h1>
+        <ContactForm onSubmit={addContact} />
+        <h2 className={css.subtitle}>Contacts</h2>
+        <Filter filter={filter} changeFilter={changeFilter} />
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={filterContacts}
+            onDeleteContact={deleteContact}
+          />
+        ) : (
+          <Message text="Contact list is empty." />
+        )}
+      </div>
+    );
+  }
+
+export default App;
